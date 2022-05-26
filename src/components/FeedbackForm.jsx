29788c2e7f34236016d2fb3fc8393d8f849d0116
Siar Wahidi <siar.wahidi@gmail.com>
2022-05-26 17:03:43 -0400

    upgraded to react router 6

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import RatingSelect from "./RatingSelect";
 import Card from "./shared/Card";
--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,6 +1,6 @@
+import React from "react";
 import { useState } from "react";
 
-import React from "react";
 function RatingSelect({ select }) {
   const [selected, setSelected] = useState(10);
 
